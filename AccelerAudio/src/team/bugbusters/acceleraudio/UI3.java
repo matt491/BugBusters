3c721fec1807a038f3bc4e43d1c3727f5022a7b8
matt491 <mattia.bovo@alice.it>
2014-05-09 14:59:57 +0200

    commit+

--- AccelerAudio/src/team/bugbusters/acceleraudio/UI3.java
+++ AccelerAudio/src/team/bugbusters/acceleraudio/UI3.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.hardware.SensorEvent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.format.DateFormat;
@@ -23,19 +22,17 @@ import android.widget.Toast;
 
 
 public class UI3 extends Activity {
-	
+
     private String datoX;
     private String datoY;
     private String datoZ;
     private ProgressBar pbX,pbY,pbZ,pb;
-	private int i=0,end_time;									//i: indice dei campioni
-	private long starttime;
+	private int i,end_time;									//i: indice dei campioni
     private double time;							//variabile usata per tenere traccia della durata della registrazione
     private String freq_curr;						
-    private String nome;								// Nome inserito dall'utente tramite EditText
+    private String nome; 							// Nome inserito dall'utente tramite EditText
     private String ts;
     private String pkg;
-    private String prefix="";
     private Button pause,resume,stop,rec,avan;									
     private EditText nome_music;						//Campo di testo del nome della registrazione
     private TextView t,varcamp;
@@ -78,7 +75,7 @@ public class UI3 extends Activity {
 		 pbY=(ProgressBar)findViewById(R.id.y_axis);
 		 pbZ=(ProgressBar)findViewById(R.id.z_axis);
 		 pb=(ProgressBar)findViewById(R.id.progressBar1);
-				 
+
 		//Visualizza il numero di campioni registrati
 		varcamp=(TextView)findViewById(R.id.campioni);
 
@@ -109,14 +106,13 @@ public class UI3 extends Activity {
             	resume.setEnabled(false);
             	pause.setEnabled(true);
             	
-            	//Creare un intent nuovo ogni volta?
         		intentToSer.putExtra("VecchioX", datoX);
         		intentToSer.putExtra("VecchioY", datoY);
         		intentToSer.putExtra("VecchioZ", datoZ);
         		intentToSer.putExtra("attFreq", freq_curr);
         		intentToSer.putExtra("attFineTempo", end_time);
         		intentToSer.putExtra("attTempo", time);
-        		intentToSer.getIntExtra("attCamp", i);
+        		intentToSer.putExtra("attCamp", i);
         		startService(intentToSer);
             		
             }
@@ -157,7 +153,7 @@ public class UI3 extends Activity {
             	pkg=getPackageName();
             	String nomeinserito=nome_music.getText().toString();
             	
-            	//Se il nome inserito e' la stringa vuota o e' gia' presente nel DB allora	
+            	//Se il nome inserito e la stringa vuota o  gia presente nel DB allora	
             	if((nomeinserito.equals("")) || sameName(nomeinserito))
             		Toast.makeText(getApplicationContext(), "Immetere un nome valido!", Toast.LENGTH_SHORT).show();
             		
@@ -199,72 +195,8 @@ public class UI3 extends Activity {
          super.onDestroy();
      }
 
-	
 
-	
-	
-	
-	//Cuore dell'activity: registra i dati memorizzandoli negli array
-	/*@Override
-	public void onSensorChanged(SensorEvent event) {
-		
-	    if(millis<prefs.getInt("duratadef", 10)) {    
-    
-			pb.setProgress((int)Math.round(millis));
-			
-			datoX.append(prefix+converti(event.values[0]));
-			datoY.append(prefix+converti(event.values[1]));
-			datoZ.append(prefix+converti(event.values[2]));
-			pbX.setProgress(Math.round(Math.abs(event.values[0])));
-			pbY.setProgress(Math.round(Math.abs(event.values[1])));
-			pbZ.setProgress(Math.round(Math.abs(event.values[2])));
-			millis=aggiornoTempo();
-			starttime=System.currentTimeMillis();
-			m=arrotondaTempo(millis);
-			t.setText("Tempo: "+m);
-			varcamp.setText(""+((i+1)*3));
-			i++;
-			prefix=" ";
-		}
-		
-		else {
-			//Adatta la dimensione della StringBuilder in base al numero di elementi presenti
-    		datoX.trimToSize();
-    		datoY.trimToSize();
-    		datoZ.trimToSize();
-        	arresto();
-		}
-		
-	}
-	
-	//Metodo per aggiornare la variabile della durata
-	protected double aggiornoTempo(){
-		return ((double)(System.currentTimeMillis() - starttime)/1000)+millis;
-	}*/
-	
-	//Metodo per arrotondare a 2 cifre decimale la durata
-	public static double arrotondaTempo(double x){
-		x = Math.floor(x*100);
-		x = x/100;
-		return x;
-		}
-	
-	public static float arrotondamento(float x){
-		x = Math.round(x*1000);
-		x = x/1000;
-		return x;
-		}
-
-	//Metodo per la conversione in short che servira' all'AudioTrack
-	public static short converti(float x){
-		if(x>32.767) return 32767;
-		if(x<-32.768) return -32768;
-		else return (short)Math.round(x*1000);
-	}
-	
-	//Metodo che controlla se e' gia' presente un NOME di una music session nel DB
-
-	//Metodo che controlla se e' gia presente un NOME di una music session nel DB
+	//Metodo che controlla se e gia presente un NOME di una music session nel DB
 	public boolean sameName(String s){
 			dbHelper.open();
 			Cursor cursor=dbHelper.fetchRecordByFilter(s);
@@ -280,7 +212,7 @@ public class UI3 extends Activity {
 			dbHelper.close();
 			return false;
 	}
-	 
+
 	//Menu Option, passa alla UI5
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -294,7 +226,7 @@ public class UI3 extends Activity {
 
 	        return true;
 	}
-	
+
 
 	   public class MyUI3Receiver extends BroadcastReceiver{
 
@@ -305,23 +237,29 @@ public class UI3 extends Activity {
 	            pbX.setProgress(intent.getIntExtra("intPbX", 0));
 	            pbY.setProgress(intent.getIntExtra("intPbY", 0));
 	            pbZ.setProgress(intent.getIntExtra("intPbZ", 0));
-	            
-	            i=intent.getIntExtra("serCamp",0);
+
+	            i=intent.getIntExtra("serCamp",1);
 	            varcamp.setText(""+i);
 	            datoX=intent.getStringExtra("ValoreX");
-	            datoY=intent.getStringExtra("ValoreX");
-	            datoZ=intent.getStringExtra("ValoreX");
+	            datoY=intent.getStringExtra("ValoreY");
+	            datoZ=intent.getStringExtra("ValoreZ");
 	            freq_curr=intent.getStringExtra("serFreq");
 	            end_time=intent.getIntExtra("serDur",0);
 	            time=intent.getDoubleExtra("serTempo", 0);
 	            t.setText("Tempo: "+time);
-	            
-	            
+
+	            if(intent.getBooleanExtra("STOP", false)){
+	            	avan.setEnabled(true);
+            		pause.setEnabled(false);
+            		stop.setEnabled(false);
+            		resume.setEnabled(false);
+	            }
+
 	        }
 
 
 	        }
 
-	
-	
+
+
 	}
