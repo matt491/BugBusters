b14168e1a64e92a05fafa8b5d7bd7cb1b778408f
Mattia <mattia.bovo@alice.it>
2014-05-15 20:11:01 +0200

    boh

--- AccelerAudio/src/team/bugbusters/acceleraudio/UI2.java
+++ AccelerAudio/src/team/bugbusters/acceleraudio/UI2.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,9 +12,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class UI2 extends Activity {
 
