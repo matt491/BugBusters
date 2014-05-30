3de7744ec8b3499afb016527352583ced496711d
Bossknife <dmloris91@gmail.com>
2014-05-30 10:55:13 +0200

    piccole cose

--- AccelerAudio/src/team/bugbusters/acceleraudio/PlayRecord.java
+++ AccelerAudio/src/team/bugbusters/acceleraudio/PlayRecord.java
@@ -60,7 +60,7 @@ public class PlayRecord extends IntentService {
 				
 		    if(riprendi) {
 		       if(at.getState()==AudioTrack.STATE_INITIALIZED && at.getPlayState()==AudioTrack.PLAYSTATE_PAUSED)   
-		    	   at.setPlaybackHeadPosition(g-100);
+		    	   at.setPlaybackHeadPosition(g-200);
 		    	   at.play();    
 		       }		
 		}	
