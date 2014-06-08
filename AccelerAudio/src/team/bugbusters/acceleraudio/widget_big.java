e45a9704e9d3a0c9cae31d84e0768a395caacc39
Bossknife <dmloris91@gmail.com>
2014-06-08 12:38:30 +0200

    modifica opzionale, ho messo la registrazione a sinistra e tutto quello che centra con la riproduzione a destra, mi sembrava più coerente senza avere tasti con funzioni simili mischiati ad altri

--- AccelerAudio/src/team/bugbusters/acceleraudio/widget_big.java
+++ AccelerAudio/src/team/bugbusters/acceleraudio/widget_big.java
@@ -177,7 +177,6 @@ public class widget_big extends AppWidgetProvider {
     		{
     			c.moveToFirst();
     			lastposition= c.getPosition();
-    			Toast.makeText(context, c.getPosition() +"" , Toast.LENGTH_SHORT).show();
     			rw.setTextViewText(R.id.title_w_big,c.getString(c.getColumnIndex(DbAdapter.KEY_NAME)));
     			rw.setTextViewText(R.id.modify_big,c.getString(c.getColumnIndex(DbAdapter.KEY_LAST)));
     			float dur=(float) (c.getInt(c.getColumnIndex(DbAdapter.KEY_DURATION)))/1000;
